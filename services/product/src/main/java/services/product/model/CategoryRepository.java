fb3f0d739cd13afcc31a36960b7f5b8b507bf037
I573021 <nicolas.klein@sap.com>
2023-04-27 14:52:17 +0200

    Rest service

--- services/product/src/main/java/services/product/model/CategoryRepository.java
+++ services/product/src/main/java/services/product/model/CategoryRepository.java
@@ -1,7 +1,13 @@
 package services.product.model;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     
+    Optional<Category> findByName(String name);
+
+    boolean existsByName(String name);
+
 }
