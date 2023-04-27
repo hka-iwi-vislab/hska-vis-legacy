b295454e8fa7347241733f352781f0155d280d0d
Julian Aßmann <julian.assmann@sap.com>
2023-04-27 14:49:01 +0200

    Add POST for products

--- services/product/src/main/java/services/product/controller/ProductController.java
+++ services/product/src/main/java/services/product/controller/ProductController.java
@@ -1,29 +1,54 @@
 package services.product.controller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 
 import services.product.model.ProductRepository;
+import services.product.model.CategoryRepository;
 import services.product.model.Product;
+import services.product.model.Category;
 
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
 
     @Autowired
-    private ProductRepository categoryRepositroy;
-    
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @GetMapping(path = "")
     public List<Product> getAllCategories(){
-        return categoryRepositroy.findAll();
+        return productRepository.findAll();
+    }
+
+    @PostMapping(path = "")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product, @RequestParam Long categoryId) {
+        Optional<Category> categoryOptional = categoryRepository.findById(categoryId);
+
+        if (categoryOptional.isPresent()) {
+            Category category = categoryOptional.get();
+            product.setCategory(category);
+            Product savedProduct = productRepository.save(product);
+
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedProduct.getId()).toUri();
+
+            return ResponseEntity.created(location).body(savedProduct);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
--- services/product/src/main/java/services/product/model/ProductRepository.java
+++ services/product/src/main/java/services/product/model/ProductRepository.java
@@ -1,9 +1,14 @@
 package services.product.model;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    
+    Optional<Product> findByName(String name);
+    Optional<Product> findByCategory(Category category);
+    Optional<Product> findByDetails(String details);
+    Optional<Product> findByPrice(double price);
 }
