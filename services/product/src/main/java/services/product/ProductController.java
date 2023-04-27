739d81d402246638bb35e5c7620bde75a5111236
Julian Aßmann <julian.assmann@sap.com>
2023-04-27 10:57:56 +0200

    Add product controller

---
+++ services/product/src/main/java/services/product/ProductController.java
@@ -0,0 +1,29 @@
+package services.product;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import services.product.model.ProductRepository;
+import services.product.model.Product;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository categoryRepositroy;
+    
+    @GetMapping(path = "")
+    public List<Product> getAllCategories(){
+        return categoryRepositroy.findAll();
+    }
+}
