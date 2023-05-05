2571b7e6857e1a6c7c434addc1a1b02837f3942f
Peter Spiegler <peter.spiegler@consiliari.de>
2023-05-05 21:38:32 +0200

    Create Database should work but doesn't. Sadge

--- monolith/sql/create-tables.sql
+++ monolith/sql/create-tables.sql
@@ -1,22 +1,9 @@
-CREATE TABLE category (
-	id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
-	PRIMARY KEY (id)
-) ENGINE=InnoDB;
-
+CREATE DATABASE IF NOT EXISTS webshop;
+USE webshop;
 CREATE TABLE role (
 	id INT NOT NULL AUTO_INCREMENT,
 	level1 INT,
 	type VARCHAR(255),
-	PRIMARY KEY (id)
-) ENGINE=InnoDB;
-
-CREATE TABLE product (
-	id INT NOT NULL AUTO_INCREMENT,
-	details VARCHAR(255),
-	name VARCHAR(255),
-	price DOUBLE,
-	category_id INT,
 	PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
@@ -34,4 +21,25 @@ CREATE UNIQUE INDEX UK_mufchskagt7e1w4ksmt9lum5l ON customer (username ASC);
 
 CREATE INDEX FK74aoh99stptslhotgf41fitt0 ON customer (role ASC);
 
+CREATE DATABASE IF NOT EXISTS products;
+USE products;
+CREATE TABLE product (
+	 id INT NOT NULL AUTO_INCREMENT,
+	 details VARCHAR(255),
+	 name VARCHAR(255),
+	 price DOUBLE,
+	 category_id INT,
+	 PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
 CREATE INDEX FK1mtsbur82frn64de7balymq9s ON product (category_id ASC);
+
+
+CREATE DATABASE IF NOT EXISTS categories;
+USE categories;
+CREATE TABLE category (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
