7d2076e5b50d2e49421765654189859add1a3473
Peter Spiegler <peter.spiegler@consiliari.de>
2023-05-15 16:22:40 +0200

    Product Categories on Navigation

--- monolith/src/main/java/hska/iwi/eShopMaster/controller/ListAllProductsAction.java
+++ monolith/src/main/java/hska/iwi/eShopMaster/controller/ListAllProductsAction.java
@@ -1,5 +1,6 @@
 package hska.iwi.eShopMaster.controller;
 
+import hska.iwi.eShopMaster.model.database.dataobjects.Category;
 import hska.iwi.eShopMaster.model.database.dataobjects.Product;
 import hska.iwi.eShopMaster.model.database.dataobjects.User;
 
@@ -8,6 +9,7 @@ import java.util.Map;
 
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import hska.iwi.eShopMaster.model.rest.CategoryManagerWithRest;
 import hska.iwi.eShopMaster.model.rest.ProductManagerWithRest;
 
 public class ListAllProductsAction extends ActionSupport {
@@ -19,6 +21,8 @@ public class ListAllProductsAction extends ActionSupport {
 	
 	User user;
 	private List<Product> products;
+
+	private List<Category> categories;
 	
 	public String execute() throws Exception{
 		String result = "input";
@@ -32,6 +36,9 @@ public class ListAllProductsAction extends ActionSupport {
 
 			ProductManagerWithRest productManagerWithRest = new ProductManagerWithRest();
 			this.products = productManagerWithRest.getProducts();
+
+			CategoryManagerWithRest categoryManagerWithRest = new CategoryManagerWithRest();
+			this.setCategories(categoryManagerWithRest.getCategories());
 
 			/*
 			ProductManager productManager = new ProductManagerImpl();
@@ -57,6 +64,13 @@ public class ListAllProductsAction extends ActionSupport {
 
 	public void setProducts(List<Product> products) {
 		this.products = products;
+	}
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
 	}
 
 }
