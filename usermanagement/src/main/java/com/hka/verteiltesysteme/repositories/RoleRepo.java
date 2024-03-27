b0f0c7a7295de25c37886b8220cff3dd7d9341ae
Maxi <maximilian.lincks@gmx.net>
2024-03-27 12:03:28 +0100

    added stuf

---
+++ usermanagement/src/main/java/com/hka/verteiltesysteme/repositories/RoleRepo.java
@@ -0,0 +1,9 @@
+package com.hka.verteiltesysteme.repositories;
+
+import com.hka.verteiltesysteme.database.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepo extends JpaRepository <Role, Integer> {
+}
---
+++ usermanagement/src/main/java/com/hka/verteiltesysteme/repositories/UserRepo.java
@@ -0,0 +1,9 @@
+package com.hka.verteiltesysteme.repositories;
+
+import com.hka.verteiltesysteme.database.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+}
